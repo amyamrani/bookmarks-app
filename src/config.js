f9b2802cbedc691574bf0b7c8e7611ee52066025
Amy Amrani <vesamrani@gmail.com>
2020-08-26 21:11:11 -0400

    updated for production deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: `http://localhost:8000/api/bookmarks/`,
+  // API_ENDPOINT: `http://localhost:8000/api/bookmarks/`,
+  API_ENDPOINT: `https://blooming-scrubland-43299.herokuapp.com/`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
